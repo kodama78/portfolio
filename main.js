ee15c4ed9f63e1a279fa6b9b574fd339129ce7ad
Shawn Otomo <kodama78@gmail.com>
2015-04-27 10:18:52 -0700

    added click function to three main divs in index page.

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 $( document ).ready(function() {
 	
 	var $leftside = $('.left_side');
+	var $rightside = $('.right_side');
+	var $maincontent = $('.main_content');
 
 	$leftside.click(function(){
 		$(this).toggleClass('left_side_active');
+		if ($rightside.hasClass('right_side_active')) {
+			$rightside.removeClass('right_side_active');
+		}
 	});
-
-	var $rightside = $('.right_side');
 
 	$rightside.click(function(){
 		$(this).toggleClass('right_side_active');
+		if ($leftside.hasClass('left_side_active')) {
+			$leftside.removeClass('left_side_active');
+		}
+	});
+
+	$maincontent.click(function(){
+		if ($leftside.hasClass('left_side_active') || $rightside.hasClass('right_side_active')) {
+			$leftside.removeClass('left_side_active');
+			$rightside.removeClass('right_side_active');
+		}
 	});
 });	
