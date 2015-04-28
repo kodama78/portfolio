acf99e78e4417d6c8f58425535a8302fec232503
Shawn Otomo <kodama78@gmail.com>
2015-04-28 16:31:25 -0700

    made jquery changes to main-content on index page. Loading images for later use.

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ $( document ).ready(function() {
 	
 	var $leftside = $('.left_side');
 	var $rightside = $('.right_side');
-	var $maincontent = $('.main_content');
+	var $maininformation = $('.main-information');
 
 	$leftside.click(function(){
 		$(this).toggleClass('left_side_active');
+		$maininformation.addClass('main-information-active');
 		if ($rightside.hasClass('right_side_active')) {
 			$rightside.removeClass('right_side_active');
 		}
@@ -13,15 +14,18 @@ $( document ).ready(function() {
 
 	$rightside.click(function(){
 		$(this).toggleClass('right_side_active');
+		$maininformation.addClass('main-information-active');
 		if ($leftside.hasClass('left_side_active')) {
 			$leftside.removeClass('left_side_active');
 		}
 	});
-
-	$maincontent.click(function(){
+	$maininformation.click(function(){
 		if ($leftside.hasClass('left_side_active') || $rightside.hasClass('right_side_active')) {
 			$leftside.removeClass('left_side_active');
 			$rightside.removeClass('right_side_active');
+		}
+		if ($maininformation.hasClass('main-information-active')) {
+			$maininformation.removeClass('main-information-active');
 		}
 	});
 });	
