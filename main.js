376f8bafd6dfa1a5a301aa57283cc3e91a6e8cdf
Shawn Otomo <kodama78@gmail.com>
2015-05-08 17:18:27 -0700

    added click function to curtain

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
+$(document).ready(function(){
+	$('.curtain_button').click(function(){
+		$('.curtain').animate({
+			opacity: 0,
+		}, 300);
+	});
 
 
-$( document ).ready(function() {
-	var $sideHandLeft = $('.side_handle_left');
-	var $leftSide = $('.left_side');
-	var $mainContent = $('.main_content');
-	var $sideHandRight = $('.side_handle_right');
-	var $rightSide = $('.right_side');
-	
-	$sideHandLeft.click(function(){
-		$(this).toggleClass('side_handle_left_active');
-		$leftSide.toggleClass('left_side_active');
-		$rightSide.removeClass('right_side_active');
-		$sideHandRight.removeClass('side_handle_right_active');
 
-	});
-	$mainContent.click(function(){
-		$leftSide.removeClass('left_side_active');
-		$sideHandLeft.removeClass('side_handle_left_active');
-		$rightSide.removeClass('right_side_active');
-		$sideHandRight.removeClass('side_handle_right_active');
-	});
-	$sideHandRight.click(function(){
-		$(this).toggleClass('side_handle_right_active');
-		$rightSide.toggleClass('right_side_active');
-		$leftSide.removeClass('left_side_active');
-		$sideHandLeft.removeClass('side_handle_left_active');
-	});
-});	
+});
