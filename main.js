a824d8aafbf72b5640ea0afabb5e95c36be43689
Shawn Otomo <kodama78@gmail.com>
2015-05-23 11:03:13 -0700

    starting over...again

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+var controller;
+
+controller = new ScrollMagic();
 $(document).ready(function(){
-	$('.curtain_button').click(function(){
-		$('.curtain').animate({
-			opacity: 0,
-		}, 300);
+	var scene1_pin = new ScrollScene({
+		triggerElement: '#scene1',
+		offset:210,
+		duration: 800
+	}).setPin('#scene1_background').addTo(controller).addIndicators();
+
+	var remove_greyscale = TweenMax.to('#scene1_background', 3, {
+		'-webkit-filter': 'grayscale(0%)',
+		'filter': 'grayscale(0%)',
+	});	
+	var fadeInName = TweenMax.from('#name', 5, {
+		opacity: 0
 	});
+	var scene1 = new ScrollScene({
+		triggerElement: '#scene1',
+	}).setTween(fadeInName).addTo(controller).addIndicators();
 
-
-
+	var scene2 = new ScrollScene({
+		triggerElement: '#scene1',
+		offset: 500,
+	}).setTween(remove_greyscale).addTo(controller).addIndicators();
 });
