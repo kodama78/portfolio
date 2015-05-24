ec2654bd69b1ec7ef70ac0f545076bee1f6ef1dc
Shawn Otomo <kodama78@gmail.com>
2015-05-24 12:50:41 -0700

    almost finished with scene 1

--- main.js
+++ main.js
@@ -8,27 +8,50 @@ $(document).ready(function(){
 	// 	duration: 800
 	// }).setPin('#scene1_background').addTo(controller).addIndicators();
 	
-	var fade_in_name_tween = TweenMax.from('#name', 3, {
-		opacity: 0
-	});
-	var fade_in_name_scene = new ScrollScene({
-		triggerElement: '#scene1',
-		offset:300
-	}).setTween(fade_in_name_tween).addTo(controller).addIndicators();
+	// var fade_in_name_tween = TweenMax.from('#name', 3, {
+	// 	opacity: 0
+	// });
+	// var fade_in_name_scene = new ScrollScene({
+	// 	triggerElement: '#scene1',
+	// 	offset:300
+	// }).setTween(fade_in_name_tween).addTo(controller).addIndicators();
+	
+	// var star_pin = new ScrollScene({
+	// 	triggerElement: '#starry_night',
+	// 	offset: 320,
+	// }).setPin('#starry_night').addTo(controller);
 
-	var star_pin = new ScrollScene({
-		triggerElement: '#scene1',
-		offset: 200,
-	}).setPin('#starry_night').addTo(controller);
+	var scene1_tween = new TimelineMax()
+		// .add(ScrollScene({triggerElement: '#starry_night', offset: 320})
+		// 	.setPin('#starry_night'), 0)
+		.add(TweenMax.to('#starry_night', 3, {
+			transform: 'scale(1)'
+		}))
+		.add(TweenMax.from('#name', 1, {
+			opacity: 0
+		}), '2')
+		.add(TweenMax.from('.intro_text', 2, {
+			opacity: 0
+		}), '6');
+		var scene1 = new ScrollScene({
+			triggerElement: '#scene1',
+			// reverse: false,
+			offset: 320
+		})
+		.setTween(scene1_tween).addTo(controller).addIndicators();
+		var star_pin = new ScrollScene({
+			triggerElement: '#starry_night',
+			offset: 320,
+		}).setPin('#starry_night').addTo(controller);
 	
-	var scale_star_tween = new TweenMax.to('#starry_night', 5, {
-		transform: 'scale(1)'
-	});
+	// var scale_star_tween = new TweenMax.to('#starry_night', 5, {
+	// 	transform: 'scale(1)'
+	// });
 	
-	var scale_star_scene = new ScrollScene({
-		triggerElement: '#scene1',
-		reverse: false,
-		offset: 400,
-		//duration: 200
-	}).setTween(scale_star_tween).addTo(controller).addIndicators();
+	// var scale_star_scene = new ScrollScene({
+	// 	triggerElement: '#scene1',
+	// 	reverse: false,
+	// 	offset: 420,
+	// 	//duration: 200
+	// }).setTween(scale_star_tween).addTo(controller).addIndicators();
 });
