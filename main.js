788cf1d17475036dbffc4ee7a97670b1c12f6838
Shawn Otomo <kodama78@gmail.com>
2015-05-24 19:28:18 -0700

    second scene started

--- main.js
+++ main.js
@@ -12,25 +12,30 @@ $(document).ready(function(){
 		}), '2')
 		.add(TweenMax.from('.intro_text', 1, {
 			opacity: 0,
-			// duration: 100
-		}), '2')
-		var scene1 = new ScrollScene({
-			triggerElement: '#scene1',
-			// reverse: false,
-			offset: 420
-		})
-		.setTween(scene1_tween).addTo(controller).addIndicators();
-		//pins the night_sky to screen
-		var star_pin = new ScrollScene({
-			triggerElement: '#starry_night',
-			offset: 320,
-			duration: 500
-		}).setPin('#starry_night').addTo(controller);
+		}),'0.5')
+	var scene1 = new ScrollScene({
+		triggerElement: '#scene1',
+		//reverse: false,
+		offset: 420
+	})
+	.setTween(scene1_tween).addTo(controller).addIndicators();
 	
-	// Might be used for scene 2
-	// var scene2_tween = new TimelineMax()
-	// 	.add(TweenMax.to('#pink_forest', 2, {
-	// 		transform
-	// 	}))
+	//pins the night_sky to screen
+	var star_pin = new ScrollScene({
+		triggerElement: '#starry_night',
+		offset: 320,
+		//duration: 500
+	}).setPin('#starry_night').addTo(controller);
+	
+	//Might be used for scene 2
+	var scene2_tween = new TimelineMax()
+		.add(TweenMax.to('.intro_text', 1, {
+			opacity: 0
+		}))
+		// .add(TweenMax.from)
+	var scene2 = new ScrollScene({
+		triggerElement: '#scene2',
+		offset: -150
+	}).setTween(scene2_tween).addTo(controller).addIndicators();
 	
 });
