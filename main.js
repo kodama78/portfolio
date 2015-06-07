942df56d4763c9601edde4654235cf61f0ae4923
Shawn Otomo <kodama78@gmail.com>
2015-06-07 10:50:54 -0700

    starting implementation of projects

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ controller = new ScrollMagic();
 
 //Global variables for star_maker function
 var skills = ['languages', 'html5', 'css3', 'javascript', 'php', 'mysql'];
-var libraries = ['libraries','bootstrap', 'jquery'];
-var productivity = ['productivity','git'];
+var libraries = ['libraries', 'bootstrap', 'jquery'];
+var productivity = ['productivity', 'git'];
 var title = ['technical', 'toolkit'];
+var projects = ['Calculator', 'Tic-Tac-Toe']
 var starfield = null;
 var offset = null;
 var half_offset = null;
@@ -15,23 +16,29 @@ var adjusted_width = null;
 var adjusted_height = null;
 var star_array = [];
 var title_stars = [];
+var project_array = [];
 $(document).ready(function() {
-	starfield = $('.star_layer');
+    starfield = $('.star_layer');
     offset = 2;
     half_offset = offset / 2;
     shadow_offset = 2;
-    adjusted_width = starfield.width()/ half_offset;
-    adjusted_height =starfield.height()/ half_offset;
+    adjusted_width = starfield.width() / half_offset;
+    adjusted_height = starfield.height() / half_offset;
 
 });
+/*
+===================================================
+STARMAKER FUNCTIONS
+===================================================
+*/
 //Skill function that will randomly place the letters into certain spots
-function star_object(span, star, letter, position_data, success_function){
+function star_object(span, star, letter, position_data, success_function) {
     this.span = span;
     this.star = star;
     this.letter = letter;
     this.position_data = position_data;
     this.success_function = success_function;
-    this.go_home = function(){
+    this.go_home = function() {
         var _this = this.span;
         var _this_star = this.star;
         var _this_letter = this.letter;
@@ -55,137 +62,186 @@ function star_object(span, star, letter, position_data, success_function){
                 }, 150);
                 _this_star.css('opacity', 0);
             })
-        }, _this_showtime);        
+        }, _this_showtime);
     }
 }
-
-function star_maker(array) {
-    var star_array = []
+function string_star_maker(array){
     for (var i = 0; i < array.length; i++) {
         var word = array[i];
-        word_string = array[0];
-        starfield = $('.'+word_string);
-        var word_div = $('<div>');
-        for (var j = 0; j < word.length; j++) {
-            var letter_string = word[j];
-            var left_random = Math.random() * offset - half_offset;
-            var top_random = Math.random() * offset - half_offset;
-            var left_offset = Math.floor(left_random * adjusted_width);
-            var top_offset = Math.floor(top_random * adjusted_height);
-            var position_data={
-                left_random: left_random,
-                top_random: top_random,
-            }
-            var span = $('<span>');
-            var twinkle_start = Math.random() * 5;
-            var star = $('<i>').addClass('fa fa-star starshining star').css({
-                'left': left_offset + 'px',
-                'top': top_offset + 'px',
-                '-webkit-animation-delay': twinkle_start + 's',
-            });
-            var letter = $('<span>', {
-                text: letter_string,
-                class: 'letter'
-            });
-            var this_star = new star_object(span, star, letter, position_data);
-
-            star_array.push(this_star);
-            span.append(star, letter);
-            word_div.append(span);
+        var word_div = $('<div>').addClass('projects_position');
+        var left_random = Math.random() * offset - half_offset;
+        var top_random = Math.random() * -offset;
+        var left_offset = Math.floor(left_random * adjusted_width);
+        var top_offset = Math.floor(top_random * adjusted_height);
+        var position_data = {
+            left_random: left_random,
+            top_random: top_random,
         }
-    starfield.append(word_div);
+        var span = $('<span>');
+        var twinkle_start = Math.random() * 5;
+        var star = $('<i>').addClass('fa fa-star starshining star').css({
+            'left': left_offset + 'px',
+            'top': top_offset + 'px',
+            '-webkit-animation-delay': twinkle_start + 's',
+        });
+        var letter = $('<span>', {
+            text: word,
+            class: 'letter'
+        });
+        var this_star = new star_object(span, star, letter, position_data);
+        project_array.push(this_star);
+        span.append(star, letter);
+        word_div.append(span);
+        $('#scene3').append(word_div);
     }
-    return star_array;
 }
+//creates the stars by running through array and then string
+function star_maker(array) {
+        var star_array = []
+        for (var i = 0; i < array.length; i++) {
+            var word = array[i];
+            word_string = array[0];
+            starfield = $('.' + word_string);
+            var word_div = $('<div>');
+            for (var j = 0; j < word.length; j++) {
+                var letter_string = word[j];
+                var left_random = Math.random() * offset - half_offset;
+                var top_random = Math.random() * offset - half_offset;
+                var left_offset = Math.floor(left_random * adjusted_width);
+                var top_offset = Math.floor(top_random * adjusted_height);
+                var position_data = {
+                    left_random: left_random,
+                    top_random: top_random,
+                }
+                var span = $('<span>');
+                var twinkle_start = Math.random() * 5;
+                var star = $('<i>').addClass('fa fa-star starshining star').css({
+                    'left': left_offset + 'px',
+                    'top': top_offset + 'px',
+                    '-webkit-animation-delay': twinkle_start + 's',
+                });
+                var letter = $('<span>', {
+                    text: letter_string,
+                    class: 'letter'
+                });
+                var this_star = new star_object(span, star, letter, position_data);
+
+                star_array.push(this_star);
+                span.append(star, letter);
+                word_div.append(span);
+            }
+            starfield.append(word_div);
+        }
+        return star_array;
+    }
+//
 //This will make fake stars that will be appended to the static star layer
 function fake_star_maker(array) {
-    for (var i = 0; i < array.length; i++) {
-        var word = skills[i];
-        for (var j = 0; j < (word.length)*5; j++) {
-            var left_random = Math.random() * offset - half_offset;
-            var top_random = Math.random() * offset - half_offset;
-            var left_offset = Math.floor(left_random * adjusted_width);
-            var top_offset = Math.floor(top_random * adjusted_height);
-            var random_scale = Math.random() * 1.3;
-            var span = $('<span>');
-            var twinkle_start = Math.random() * 5;
-            var star = $('<i>').addClass('fa fa-star starshining star').css({
-                'left': left_offset + 'px',
-                'top': top_offset + 'px',
-                'transform': 'scale(' + random_scale +')',
-                '-webkit-animation-delay': twinkle_start + 's',
-            });
-            span.append(star);
-            $('.star_layer1').append(span);
+        for (var i = 0; i < array.length; i++) {
+            var word = skills[i];
+            for (var j = 0; j < (word.length) * 5; j++) {
+                var left_random = Math.random() * offset - half_offset;
+                var top_random = Math.random() * offset - half_offset;
+                var left_offset = Math.floor(left_random * adjusted_width);
+                var top_offset = Math.floor(top_random * adjusted_height);
+                var random_scale = Math.random() * 1.3;
+                var span = $('<span>');
+                var twinkle_start = Math.random() * 5;
+                var star = $('<i>').addClass('fa fa-star starshining star').css({
+                    'left': left_offset + 'px',
+                    'top': top_offset + 'px',
+                    'transform': 'scale(' + random_scale + ')',
+                    '-webkit-animation-delay': twinkle_start + 's',
+                });
+                span.append(star);
+                $('.star_layer1').append(span);
+            }
         }
     }
-}
 //Calls the star_mover function to move the created stars
-function shooting_star(star_array){
-    for(var i = 0; i < star_array.length; i++){
+function shooting_star(star_array) {
+    for (var i = 0; i < star_array.length; i++) {
         star_array[i].go_home();
     }
 }
-for (var i = 0; i < 1; i++){
-    var tiny_c = $('<div>').addClass('tiny stone');
-    var xs_c = $('<div>').addClass('xs stone');
-   var small_c = $('<div>').addClass('small stone');
-    //var medium_c = $('<div>').addClass('medium');
-    //var large_c = $('<div>').addClass('large');
-    
-    //xs_c.append(tiny_c);
-    //small_c.append(xs_c);
-    //medium_c.append(small_c);
-    //large_c.append(medium_c);
-    $('#scene3').append(tiny_c, xs_c, small_c);
+
+/*
+===================================================
+RIPPLE FUNCTIONS
+===================================================
+*/
+//creates divs for ripples
+function ripple_maker() {
+    for (var i = 0; i < 1; i++) {
+        var tiny_c = $('<div>').addClass('tiny stone');
+        var xs_c = $('<div>').addClass('xs stone');
+        var small_c = $('<div>').addClass('small stone');
+        //var medium_c = $('<div>').addClass('medium');
+        //var large_c = $('<div>').addClass('large');
+
+        //xs_c.append(tiny_c);
+        //small_c.append(xs_c);
+        //medium_c.append(small_c);
+        //large_c.append(medium_c);
+        $('#scene3').append(tiny_c, xs_c, small_c);
+    }
 }
 
-function ripples(){
-    $('.calculator').addClass('word_move');
-    setTimeout(function(){
-        $('.calculator').addClass('word_stone');
-    }, 1000);
-    
-    setTimeout(function(){
+//applies the classes to the divs in ripplemaker to create ripple effect
+function ripples() {
+    // $('.calculator').addClass('word_move');
+    // setTimeout(function() {
+    //     $('.calculator').addClass('word_stone');
+    // }, 1000);
+    setTimeout(function() {
         $('.tiny').addClass('tiny_stone');
     }, 1200);
-    
-    setTimeout(function(){
-      $('.xs').addClass('xs_stone');  
+
+    setTimeout(function() {
+        $('.xs').addClass('xs_stone');
     }, 1270);
-    
-    setTimeout(function(){
-      $('.small').addClass('small_stone');  
+
+    setTimeout(function() {
+        $('.small').addClass('small_stone');
     }, 2000);
-     setTimeout(function(){
-      $('.small').addClass('small_stone_gone');  
+    setTimeout(function() {
+        $('.small').addClass('small_stone_gone');
     }, 3000);
-    
+
 }
+
 $(document).ready(function() {
+    string_star_maker(projects);
+    ripple_maker(); 
     //creates the static stars on star_layer1
     fake_star_maker(skills);
+
     //creates the animated stars that will create the letters for each set of arrays
     title_stars = star_maker(title);
     skills_stars = star_maker(skills);
     libraries_stars = star_maker(libraries);
     productivity_stars = star_maker(productivity);
+    /*
+    ========================================================
+    SCENE 1
+    ========================================================
+    */
     //Adds scaling and fade ins for header text and intro text
     var scene1_tween = new TimelineMax()
         .add(TweenMax.to('#starry_night', 2, {
             transform: 'scale(1)'
         }))
         .add(TweenMax.to('.star_layer1', 2, {
-        	transform: 'scale(1)'
+            transform: 'scale(1)'
         }), '0')
         .add(TweenMax.from('.intro_text', 0.5, {
             opacity: 0,
         }), '0.5')
     var scene1 = new ScrollScene({
-            triggerElement: '#scene1',
-            reverse: false,
-            offset: 400
-        }).setTween(scene1_tween).addTo(controller).addIndicators();
+        triggerElement: '#scene1',
+        reverse: false,
+        offset: 400
+    }).setTween(scene1_tween).addTo(controller).addIndicators();
 
     //pins the night_sky to screen
     var scene1_pin = new ScrollScene({
@@ -194,7 +250,11 @@ $(document).ready(function() {
         //duration: 500
     }).setPin('#starry_night').addTo(controller);
 
-    //Scene 2
+    /*
+    ========================================================
+    SCENE 2
+    ========================================================
+    */
     var scene2_tween = new TimelineMax()
         .add(TweenMax.to('.intro_text', 1, {
             opacity: 0
@@ -215,15 +275,15 @@ $(document).ready(function() {
         triggerElement: '#scene2',
         reverse: false,
         offset: -250
-    }).on('start', function(){
+    }).on('start', function() {
         shooting_star(title_stars);
     }).addTo(controller).addIndicators();
-    
-     var skills_mover = new ScrollScene({
+
+    var skills_mover = new ScrollScene({
         triggerElement: '#scene2',
         reverse: false,
         offset: -190
-    }).on('start', function(){
+    }).on('start', function() {
         shooting_star(skills_stars);
     }).addTo(controller).addIndicators();
 
@@ -231,7 +291,7 @@ $(document).ready(function() {
         triggerElement: '#scene2',
         reverse: false,
         offset: -170
-    }).on('start', function(){
+    }).on('start', function() {
         shooting_star(libraries_stars);
     }).addTo(controller).addIndicators();
 
@@ -239,7 +299,7 @@ $(document).ready(function() {
         triggerElement: '#scene2',
         reverse: false,
         offset: -150
-    }).on('start', function(){
+    }).on('start', function() {
         shooting_star(productivity_stars);
     }).addTo(controller).addIndicators();
     //pins the star layer to #starry_night
@@ -247,10 +307,27 @@ $(document).ready(function() {
         triggerElement: '#scene2',
         offset: -170,
         duration: 800
-    }).setPin('.star_layer',{
+    }).setPin('.star_layer', {
         pushFollowers: true
     }).addTo(controller);
-
+    /*
+    ========================================================
+    SCENE 3
+    ========================================================
+    */
+    var project_mover = new ScrollScene({
+        triggerElement: '#scene3',
+        offset: 50,
+    }).on('start', function(){
+        shooting_star(project_array);
+    }).addTo(controller).addIndicators();
+    //ripples the words
+    var ripple_start = new ScrollScene({
+        triggerElement: '#scene3',
+        offset: 50,
+    }).on('start', function() {
+        ripples();
+    }).addTo(controller).addIndicators();
     // var scene3_tween = new TimelineMax()
     //     .add(TweenMax.from('.small_dream', 1, {
     //         opacity: 0,
